a0ff96f2c98c9e515074ff3d965b59cbe36d5aee
Mek Karpeles <michael.karpeles@gmail.com>
2016-04-23 03:39:58 +0000

    Versioning APIs and adding pragma

--- server/views/apis/__init__.py
+++ server/views/apis/__init__.py
@@ -10,35 +10,11 @@
     :license: see LICENSE for more details.
 """
 
-from flask import render_template, request
-from flask.views import MethodView
-from views import rest_api
-from views.apis.v1 import items, collections, search, wayback, iiif, music, \
-    books, analytics, television, scholar, webtorrent
-from util import domain
 
+from views.apis import v2
 
-class Endpoints(MethodView):
-    @rest_api
-    def get(self, uri=None):      
-        urlbase = request.base_url[:-1]
-        return dict([(urls[i+1].__name__.split(".")[-1].lower(),
-                      urlbase + urls[i])
-                     for i in range(len(urls))[::2]]
-)
 
 urls = (
-    '/items', items,
-    '/search', search.Search,
-    '/collections', collections,
-    '/snapshots', wayback,
-    '/music', music,
-    '/iiif', iiif,
-    '/scholar', scholar,
-    '/books', books,
-    '/analytics', analytics,
-    '/webtorrents', webtorrent,
-    '/television', television,
-    '/', Endpoints
+    '', v2
     )
 
---
+++ server/views/apis/v2/__init__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*-coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+    URLs list for api.v1
+
+    :copyright: (c) 2015 by Internet Archive
+    :license: see LICENSE for more details.
+"""
+
+from flask import request
+from flask.views import MethodView
+from views import rest_api
+from views.apis.v2 import items, collections, search, wayback, iiif, pragma, \
+    music, books, analytics, television, scholar
+
+
+class Endpoints(MethodView):
+    @rest_api
+    def get(self, uri=None):      
+        urlbase = request.base_url[:-1]
+        return dict([(urls[i+1].__name__.split(".")[-1].lower(),
+                      urlbase + urls[i])
+                     for i in range(len(urls))[::2]])
+
+
+urls = (
+    '/items', items,
+    '/search', search.Search,
+    '/collections', collections,
+    '/snapshots', wayback,
+    '/music', music,
+    '/iiif', iiif,
+    '/pragma', pragma,
+    '/scholar', scholar,
+    '/books', books,
+    '/analytics', analytics,
+    #'/television', television,
+    '/', Endpoints
+)
